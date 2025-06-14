4489e78fa195e6644852fb7fe9da7b86bf63608c
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-06-14 15:57:13 +0200

    Update karatsuba_algorithm.py

--- algo-course/stanford-algs/course1/assignment1/karatsuba-algorithm/karatsuba_algorithm.py
+++ algo-course/stanford-algs/course1/assignment1/karatsuba-algorithm/karatsuba_algorithm.py
@@ -13,19 +13,3 @@ def multiply(x,y):
     B = multiply(x1+x0, y1+y0) - A - C
     
     return A*(m**2) + B * m + C
-
-lst = [(3141592653589793238462643383279502884197169399375105820974944592, 2718281828459045235360287471352662497757247093699959574966967627)]
-for _ in range(100):
-    n = random.randint(1, 1000)
-    lst.append((random.randint(10**n, 10**(n+1)-1),random.randint(10**n, 10**(n+1)-1)))
-    
-fail = False 
-count = 0
-for x, y in lst:
-    count+=1
-    ans = multiply(x,y)
-    
-    if x*y != ans:
-        fail = True
-        break
-print("Fail" if fail else "Success!!", count)
