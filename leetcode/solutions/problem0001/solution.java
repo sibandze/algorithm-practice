d228c0af2ffe2272eb20c7c7b530f81593483ede
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-06-30 10:15:07 +0200

    Update solution.java

---
+++ leetcode/solutions/problem0001/solution.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> num_map = new HashMap<>();
+        for (int idx = 0; idx < nums.length; idx++) {
+            int target_num = target - nums[idx];
+            if (num_map.containsKey(target_num)) {
+                return new int[] { num_map.get(target_num), idx };
+            }
+            num_map.put(nums[idx], idx);
+        }
+        return new int[] {0,0};
+    }
+}
