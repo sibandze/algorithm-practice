1b9a8122211329ac13385a9070898db6223d5e47
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-06-30 10:16:39 +0200

    Update solution.py

--- leetcode/solutions/problem0001/solution.py
+++ leetcode/solutions/problem0001/solution.py
@@ -1,1 +1,9 @@
 # Solution for problem 1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for idx in range(len(nums)):
+            target_num = target-nums[idx]
+            if target_num in num_map:
+                return [num_map[target_num], idx]
+            num_map[nums[idx]] = idx
