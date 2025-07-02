f6dd4c5f9a34a78a454436ba1eb69eabd61e72e1
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-07-02 13:30:31 +0200

    Update solution.py

--- leetcode/solutions/problem0005/solution.py
+++ leetcode/solutions/problem0005/solution.py
@@ -1,1 +1,22 @@
-# Solution for problem 5
+# Leetcode 3 Longest Palindrome Substring
+# Given a string s, return the longest palindromic substring in s.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        longest_palindrome = [1, [0, 0]]
+
+        def expand_around_center(left, right):
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+            length = right - left - 1  # calculate length after loop
+            if length > longest_palindrome[0]:
+                longest_palindrome[0] = length
+                longest_palindrome[1] = [left + 1, right - 1]
+
+        for i in range(n):
+            expand_around_center(i, i)  # odd-length palindrome
+            expand_around_center(i, i + 1)  # even-length palindrome
+
+        return s[longest_palindrome[1][0]:longest_palindrome[1][1] + 1]
