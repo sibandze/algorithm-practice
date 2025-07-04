47fe6de161e9c106fe9da82f5c5ff40067062b0f
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-07-04 16:40:08 +0200

    Update solution.py leetcode 15

--- leetcode/solutions/problem0015/solution.py
+++ leetcode/solutions/problem0015/solution.py
@@ -1,1 +1,31 @@
-# Solution for problem 15
+# Leetcode 15 3Sum
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+# Notice that the solution set mushh not contain duplicate triplets 
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            if nums[i] + nums[i + 1] + nums[i + 2] > 0:
+                break
+            if nums[i] + nums[-1] + nums[-2] < 0:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    ans.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return ans# Solution for problem 15
