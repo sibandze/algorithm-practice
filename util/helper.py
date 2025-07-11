7cc971ed80767365bc30560390b3ce16aa4c7927
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-07-11 11:12:45 +0200

    Update helper.py

--- util/helper.py
+++ util/helper.py
@@ -5,5 +5,5 @@ directory = pathlib.Path("~/storage/downloads").expanduser()
 
 for file in directory.glob("*algo-*.pdf"):
     new_filename = re.sub(r'^.*(algo-)', r'algo-', file.name)
-    #file.rename(file.parent / new_filename)
+    file.rename(file.parent / new_filename)
     print(f"Renamed {file.name} to {new_filename}")
