d67e3d7e8e925bad147998e2345b07532aee7846
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-07-11 11:42:57 +0200

    Update helper.py

--- util/helper.py
+++ util/helper.py
@@ -2,8 +2,15 @@ import pathlib
 import re
 
 directory = pathlib.Path("~/storage/downloads").expanduser()
+start = input().strip()  # strip whitespace
+if not start:
+    return
 
-for file in directory.glob("*algo-*.pdf"):
-    new_filename = re.sub(r'^.*(algo-)', r'algo-', file.name)
+pattern = rf'^.*({re.escape(start)}.*\.pdf)$'
+for file in directory.glob(f"*{start}*.pdf"):
+    new_filename = re.sub(pattern, r'\1', file.name)
+    if file.name == new_filename:
+        print(f"{file.name} already saved properly.")
+        continue
     file.rename(file.parent / new_filename)
     print(f"Renamed {file.name} to {new_filename}")
