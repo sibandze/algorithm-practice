55d347f0c2433303d20e1cdcd104981174d417e9
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-07-15 18:48:04 +0200

    Update solution.py

--- leetcode/solutions/problem3208/solution.py
+++ leetcode/solutions/problem3208/solution.py
@@ -1,25 +1,24 @@
-# Solution for problem 3208
+'''
+3208 Altering Groups II
+here is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i]:
+
+colors[i] == 0 means that tile i is red.
+colors[i] == 1 means that tile i is blue.
+An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
+
+Return the number of alternating groups.
+
+Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+'''
 class Solution:
-	def numberOfAlternatingGroups(self, colors, k):
-		i = 0, j = 1
-		count = 0
-		n = len(colors)
-		while j-i < k and j<n:
-			if colors[j] == colors[j-1]:
-				i = j
-			elif j - i + 1 = k:
-				count+=1
-			j+=1
-		while j<n:
-			if colors[j] == colors[j-1]:
-				i = j
-				j+=1
-				while j-i < k and j<n:
-					if colors[j] == colors[j-1]:
-						i=j
-					j+=1
-			else:
-				count += 1
-				j+=1
-				i+=1
-		return count
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        n = len(colors)
+        count = 0
+        i = 0
+        for j in range(1, n+k-1):
+            if colors[j%n] == colors[(j-1)%n]:
+                i = j
+            elif j - i + 1== k :
+                count += 1
+                i += 1
+        return count
