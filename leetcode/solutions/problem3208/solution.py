5495a420d960c305f2236863651c0ae3e0e8b056
Sibandze <nkosingphiless@gmail.com>
2025-07-15 17:29:57 +0200

    updated leetcode 3208 solution

--- leetcode/solutions/problem3208/solution.py
+++ leetcode/solutions/problem3208/solution.py
@@ -1,1 +1,25 @@
 # Solution for problem 3208
+class Solution:
+	def numberOfAlternatingGroups(self, colors, k):
+		i = 0, j = 1
+		count = 0
+		n = len(colors)
+		while j-i < k and j<n:
+			if colors[j] == colors[j-1]:
+				i = j
+			elif j - i + 1 = k:
+				count+=1
+			j+=1
+		while j<n:
+			if colors[j] == colors[j-1]:
+				i = j
+				j+=1
+				while j-i < k and j<n:
+					if colors[j] == colors[j-1]:
+						i=j
+					j+=1
+			else:
+				count += 1
+				j+=1
+				i+=1
+		return count
