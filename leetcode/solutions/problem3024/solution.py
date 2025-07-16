7804c03d534d7b1463e47ebb905ff94d415556b9
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-07-16 17:35:14 +0200

    Update solution.py

--- leetcode/solutions/problem3024/solution.py
+++ leetcode/solutions/problem3024/solution.py
@@ -11,19 +11,16 @@ Return a string representing the type of triangle that can be formed or "none" i
 '''
 class Solution:
     def triangleType(self, nums: List[int]) -> str:
-        if nums[1] == nums[2] == nums[0]:
-            return 'equilateral'
         nums.sort()
-        if nums[0] == nums[1] or nums[1] == nums[2]:
-            if (nums[0] == nums[1] and nums[2] < nums[1] + nums[0]) \
-               or (nums[2] == nums[1]):
-               return 'isosceles'
-            else:
-                return 'none'
-        elif nums[0] + nums[1] > nums[2] and nums[1] + nums[2] > nums[0] and nums[0] + nums[2] > nums[1]:
-            return 'scalene'
-        else:
-            return 'none'
 
+        if nums[0] + nums[1] <= nums[2]:
+            return "none"
+
+        if nums[0] == nums[1] and nums[1] == nums[2]:
+            return "equilateral"
+        elif nums[0] == nums[1] or nums[1] == nums[2]: 
+            return "isosceles"
+        else:
+            return "scalene"
 
         
